89dbbbfd6e5bb0b14683e4a74b7b76abe7a62bbe
Matt Newbigging <mnewbigging@opus2.com>
2020-11-08 13:44:13 +0000

    installed mobx

--- src/playground/Playground.tsx
+++ src/playground/Playground.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import './playground.scss';
+
 export class Playground extends React.Component {
   public render() {
-    return <div>hi there</div>;
+    return (
+      <div className={'app-list-container'}>
+        <div className={''}></div>
+      </div>
+    );
   }
 }

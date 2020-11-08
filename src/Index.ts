9ec2828e107c6369e357a3cdf9237b71c5556128
Matt Newbigging <mnewbigging@opus2.com>
2020-11-08 13:33:52 +0000

    setup

---
+++ src/Index.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Playground } from './playground/Playground';
+
+ReactDOM.render(React.createElement(Playground), document.getElementById("app"));
---
+++ src/playground/Playground.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class Playground extends React.Component {
+  public render() {
+    return <div>hi there</div>;
+  }
+}

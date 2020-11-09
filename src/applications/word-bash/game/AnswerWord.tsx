31029732ed5c4aa8010c3c5dcb2afd7b0fab4faa
Matt Newbigging <mnewbigging@opus2.com>
2020-11-09 22:21:07 +0000

    tidy

--- src/applications/word-bash/game/AnswerWord.tsx
+++ src/applications/word-bash/game/AnswerWord.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import { LetterTileStatus } from '../WordBashState';
+import { LetterTile } from './LetterTile';
+
+import './answer-word.scss';
+
 interface AnswerWordProps {
   word: string;
 }
@@ -7,6 +12,14 @@ interface AnswerWordProps {
 export class AnswerWord extends React.Component<AnswerWordProps> {
   public render() {
     const { word } = this.props;
-    return <div className={'answer-word'}>{word}</div>;
+    const upperWord = word.toUpperCase();
+    const tiledWord: JSX.Element[] = [];
+    for (let i: number = 0; i < word.length; i++) {
+      tiledWord.push(
+        <LetterTile key={'lt-' + i} letter={upperWord[i]} status={LetterTileStatus.NORMAL} />
+      );
+    }
+
+    return <div className={'answer-word'}>{tiledWord}</div>;
   }
 }

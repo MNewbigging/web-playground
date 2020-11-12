fedf8ae43e1081611a6fba3abb4971e674324d9f
Matt Newbigging <mnewbigging@opus2.com>
2020-11-12 23:53:12 +0000

    answer pools responsive

--- src/applications/word-bash/game/AnswerWordZone.tsx
+++ src/applications/word-bash/game/AnswerWordZone.tsx
@@ -14,42 +14,82 @@ interface AWZProps {
 @observer
 export class AnswerWordZone extends React.Component<AWZProps> {
   public render() {
-    const answers: JSX.Element[][] = this.getAnswerWords();
-
-    return (
-      <div className={'answer-word-zone'}>
-        <div className={'answer-pool'}>{answers[0]}</div>
-        <div className={'answer-pool'}>{answers[1]}</div>
-        <div className={'answer-pool'}>{answers[2]}</div>
-        <div className={'answer-pool'}>{answers[3]}</div>
-      </div>
-    );
+    return <div className={'answer-word-zone'}>{this.getAnswerWords()}</div>;
   }
 
-  // This rebuilds/redraws all answers each time one is
+  // Gets answer pools for each tier of answer that exists
   private getAnswerWords() {
+    const answerPools: JSX.Element[] = [];
+    // Check for each tier
+    const ans3to4 = this.getAnswers3To4();
+    if (ans3to4.length) {
+      answerPools.push(
+        <div key={'ap1'} className={'answer-pool'}>
+          {ans3to4}
+        </div>
+      );
+    }
+    const ans5to6 = this.getAnswers5To6();
+    if (ans5to6.length) {
+      answerPools.push(
+        <div key={'ap2'} className={'answer-pool'}>
+          {ans5to6}
+        </div>
+      );
+    }
+    const ans7to8 = this.getAnswers7To8();
+    if (ans7to8.length) {
+      answerPools.push(
+        <div key={'ap3'} className={'answer-pool'}>
+          {ans7to8}
+        </div>
+      );
+    }
+    const ans9p = this.getAnswers9Plus();
+    if (ans9p.length) {
+      answerPools.push(
+        <div key={'ap4'} className={'answer-pool'}>
+          {ans9p}
+        </div>
+      );
+    }
+
+    return answerPools;
+  }
+
+  private getAnswers3To4() {
     const { wbState } = this.props;
     const answers3To4: JSX.Element[] = [];
-    const answers5To6: JSX.Element[] = [];
-    const answers7To8: JSX.Element[] = [];
-    const answers9Plus: JSX.Element[] = [];
-
     wbState.answers3To4.forEach((ans) => {
-      answers3To4.push(<AnswerWord word={ans} wbState={wbState} />);
+      answers3To4.push(<AnswerWord key={'aw-' + ans} word={ans} wbState={wbState} />);
     });
+    return answers3To4;
+  }
 
+  private getAnswers5To6() {
+    const { wbState } = this.props;
+    const answers5To6: JSX.Element[] = [];
     wbState.answers5To6.forEach((ans) => {
-      answers5To6.push(<AnswerWord word={ans} wbState={wbState} />);
+      answers5To6.push(<AnswerWord key={'aw-' + ans} word={ans} wbState={wbState} />);
     });
+    return answers5To6;
+  }
 
+  private getAnswers7To8() {
+    const { wbState } = this.props;
+    const answers7To8: JSX.Element[] = [];
     wbState.answers7To8.forEach((ans) => {
-      answers7To8.push(<AnswerWord word={ans} wbState={wbState} />);
+      answers7To8.push(<AnswerWord key={'aw-' + ans} word={ans} wbState={wbState} />);
     });
+    return answers7To8;
+  }
 
+  private getAnswers9Plus() {
+    const { wbState } = this.props;
+    const answers9Plus: JSX.Element[] = [];
     wbState.answers9Plus.forEach((ans) => {
-      answers9Plus.push(<AnswerWord word={ans} wbState={wbState} />);
+      answers9Plus.push(<AnswerWord key={'aw-' + ans} word={ans} wbState={wbState} />);
     });
-
-    return [answers3To4, answers5To6, answers7To8, answers9Plus];
+    return answers9Plus;
   }
 }

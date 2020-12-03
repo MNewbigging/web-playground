41334919a00a5bf921587246a798f0294126f49f
Matt Newbigging <mnewbigging@opus2.com>
2020-12-03 18:11:20 +0000

    styling main game screen

--- src/applications/word-bash/menu/WBOptions.tsx
+++ src/applications/word-bash/menu/WBOptions.tsx
@@ -5,22 +5,39 @@ import { Button } from '@blueprintjs/core';
 import { WBScreen } from '../fixed';
 import { WordBashState } from '../WordBashState';
 
+import './wb-options.scss';
+
 interface WBOProps {
   wbState: WordBashState;
 }
 
 export class WBOptions extends React.Component<WBOProps> {
   public render() {
+    return (
+      <div className={'wb-options'}>
+        <div className={'back-wall'}></div>
+        <div className={'desk'}></div>
+        <div className={'computer'}>
+          <div className={'computer-screen'}></div>
+        </div>
+        <div className={'computer-base'}></div>
+        <div className={'monitor'}>
+          <div className={'monitor-screen'}></div>
+        </div>
+        <div className={'monitor-stand'}></div>
+        <div className={'monitor-stand-foot'}></div>
+      </div>
+    );
+  }
+
+  private renderBackToMenu() {
     const { wbState } = this.props;
     return (
-      <>
-        <div>I am the options screen</div>
-        <Button
-          className={'button'}
-          text={'BACK'}
-          onClick={() => wbState.toWbScreen(WBScreen.MENU)}
-        />
-      </>
+      <Button
+        className={'button'}
+        text={'BACK'}
+        onClick={() => wbState.toWbScreen(WBScreen.MENU)}
+      />
     );
   }
 }

b834ecfb724155d792fc4400cb2d70e8ba13136a
Matt Newbigging <mnewbigging@opus2.com>
2020-12-16 21:33:25 +0000

    styled word bash card, changed font styling across app

--- src/applications/word-bash/game/LetterTile.tsx
+++ src/applications/word-bash/game/LetterTile.tsx
@@ -15,13 +15,6 @@ interface LetterTileProps extends ILetterTile {
 
 @observer
 export class LetterTile extends React.Component<LetterTileProps> {
-  private landingSFX: HTMLAudioElement;
-
-  constructor(props: LetterTileProps) {
-    super(props);
-    this.landingSFX = new Audio(this.pickRandomLandingSFX());
-  }
-
   public render() {
     const { delay, letter, status, innerClass, outerClass } = this.props;
 
@@ -32,10 +25,6 @@ export class LetterTile extends React.Component<LetterTileProps> {
     const style = {
       animationDelay: `${delay}s`,
     };
-
-    // Play tile landing sfx after anim and delay
-    const animEndTime = (0.5 + delay) * 1000;
-    setTimeout(this.onAnimationEnd, animEndTime);
 
     const outerClasses: string[] = ['letter-tile', 'fall-in', outerClass];
     const innerClasses: string[] = ['lt-inner', status, innerClass];
@@ -51,17 +40,5 @@ export class LetterTile extends React.Component<LetterTileProps> {
 
   private onInnerClick = () => {
     this.props.select?.();
-  };
-
-  private pickRandomLandingSFX(): string {
-    const rnd = Math.ceil(Math.random() * 4);
-    return '/dist/assets/sfx/impactWood_heavy_00' + rnd + '.ogg';
-  }
-
-  private onAnimationEnd = () => {
-    // const sfxClip = 'impactWood_heavy_000.ogg';
-    // const dropFx = new Audio('/dist/assets/sfx/' + sfxClip);
-    // dropFx.play();
-    this.landingSFX.play();
   };
 }

7c23cebfdf85150bc362105172e2f5585b3a15ff
Matt Newbigging <mnewbigging@opus2.com>
2021-01-02 14:11:27 +0000

    memo-rune app card styling

--- src/playground/AppList.tsx
+++ src/playground/AppList.tsx
@@ -7,6 +7,7 @@ import './app-list.scss';
 // Import the styles for each app card from their application folder
 import '../applications/desk-scene/desk-scene-card.scss';
 import '../applications/word-bash/word-bash-card.scss';
+import '../applications/memo-rune/memo-rune-card.scss';
 
 interface AppListProps {
   pgState: PlaygroundState;

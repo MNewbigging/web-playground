1ac60d5b610837e1a5928311c4d4ac969cb42ca8
Matt Newbigging <mnewbigging@opus2.com>
2021-01-10 14:57:04 +0000

    basic classes and menu holder in place

--- src/applications/connect-four/ConnectFour.tsx
+++ src/applications/connect-four/ConnectFour.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import { observer } from 'mobx-react';
+
+import { Button, Card } from '@blueprintjs/core';
+
+import { ConnectFourState } from './ConnectFourState';
+
+import './connect-four.scss';
+
 interface CFProps {
   toApp: () => void;
 }
 
+@observer
 export class ConnectFour extends React.Component<CFProps> {
+  private cfState = new ConnectFourState();
+
   public render() {
-    return <div>I AM CONNECT FOUR!</div>;
+    const toRender: JSX.Element[] = [];
+    toRender.push(this.renderMenuButtons());
+
+    return <div className={'connect-four'}>{toRender}</div>;
+  }
+
+  private renderMenuButtons() {
+    const { toApp } = this.props;
+    return (
+      <Card key={'c4-menu-card'} elevation={2} className={'c4-menu'}>
+        <Button text={'Exit'} onClick={() => toApp()} />
+      </Card>
+    );
   }
 }

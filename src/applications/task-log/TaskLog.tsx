c2dc93f5201292c5bfc359c4889eb4c7f7b368ef
Matt Newbigging <mnewbigging@opus2.com>
2021-02-06 00:03:07 +0000

    did clock

--- src/applications/task-log/TaskLog.tsx
+++ src/applications/task-log/TaskLog.tsx
@@ -1,18 +1,23 @@
+import { observer } from 'mobx-react';
 import React from 'react';
 
 import { TLHeader } from './components/TLHeader';
 import { TLPanel } from './components/TLPanel';
+import { TaskLogState } from './TaskLogState';
 
 import './task-log.scss';
 
+@observer
 export class TaskLog extends React.Component {
+  private readonly tlState = new TaskLogState();
   public render() {
     return (
       <div className={'tl-background'}>
-        <TLHeader time={'20:47'} />
+        <TLHeader time={this.tlState.timeStr} />
         <TLPanel className={'tl-navbar'}>
           <div>I'm the panel content</div>
         </TLPanel>
+        <div className={'tl-content'}></div>
       </div>
     );
   }
---
+++ src/applications/task-log/TaskLogState.ts
@@ -0,0 +1,29 @@
+import { observable } from 'mobx';
+
+export class TaskLogState {
+  @observable.ref public timeStr: string;
+
+  constructor() {
+    this.runClock();
+  }
+
+  private runClock() {
+    const time = new Date();
+    let hours = time.getHours().toString();
+    let mins = time.getMinutes().toString();
+
+    if (hours.length < 2) {
+      hours = '0' + hours;
+    }
+
+    if (mins.length < 2) {
+      mins = '0' + mins;
+    }
+
+    this.timeStr = `${hours}:${mins}`;
+
+    // Find how many seconds til next minute passes
+    const remainingSeconds = 60 - time.getSeconds();
+    setTimeout(() => this.runClock(), remainingSeconds * 1000);
+  }
+}

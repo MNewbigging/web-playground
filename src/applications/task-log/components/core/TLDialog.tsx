44415b0f93c6527374bb4dd777d06821e977f625
Matt Newbigging <mnewbigging@opus2.com>
2021-02-08 20:54:35 +0000

    added backdrop to dialogs to prevent outside click

--- src/applications/task-log/components/core/TLDialog.tsx
+++ src/applications/task-log/components/core/TLDialog.tsx
@@ -19,23 +19,26 @@ interface DialogProps {
 
 export const TLDialog: React.FC<DialogProps> = ({ children, state, title, onCancel, onAccept }) => {
   return (
-    <div className={'tl-dialog ' + state}>
-      <div className={'header'}>
-        <div className={'title'}>{title}</div>
-        <hr />
+    <>
+      <div className={'tl-dialog-backdrop ' + state}></div>
+      <div className={'tl-dialog ' + state}>
+        <div className={'header'}>
+          <div className={'title'}>{title}</div>
+          <hr />
+        </div>
+        <div className={'children'}>{children}</div>
+        <div className={'footer'}>
+          <TLButton text={'CANCEL'} intent={TLButtonIntent.REJECT} onClick={onCancel} />
+          <TLButton
+            text={'ACCEPT'}
+            intent={TLButtonIntent.ACCEPT}
+            onClick={() => {
+              onAccept();
+              onCancel();
+            }}
+          />
+        </div>
       </div>
-      <div className={'children'}>{children}</div>
-      <div className={'footer'}>
-        <TLButton text={'CANCEL'} intent={TLButtonIntent.REJECT} onClick={onCancel} />
-        <TLButton
-          text={'ACCEPT'}
-          intent={TLButtonIntent.ACCEPT}
-          onClick={() => {
-            onAccept();
-            onCancel();
-          }}
-        />
-      </div>
-    </div>
+    </>
   );
 };

b5908b518f2d7043b6bb11042fb8f022b7c0c084
Matt Newbigging <mnewbigging@opus2.com>
2021-02-19 19:30:19 +0000

    styling

--- src/applications/blether/screens/chat/mobile/BLChatMobile.tsx
+++ src/applications/blether/screens/chat/mobile/BLChatMobile.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import './bl-chat-mobile.scss';
+
 export class BLChatMobile extends React.PureComponent {
   public render() {
-    return <div>CHAT MOBILE</div>;
+    return (
+      <div className={'chat-mobile'}>
+        <div className={'top-bar'}>
+          <div className={'name'}>Group name</div>
+          <div className={'drawer button small'}>...</div>
+        </div>
+        <div className={'log'}></div>
+        <div className={'input-area'}>
+          <input className={'input'} type={'text'} />
+        </div>
+      </div>
+    );
   }
 }
